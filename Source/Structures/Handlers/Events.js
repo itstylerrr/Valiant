057c4c37f26f7f8b17874d1f8698bcd396e75ffd
tyler <63240641+itstylerrr@users.noreply.github.com>
2022-06-15 20:24:24 -0500

    🤚 Create event handler.

---
+++ Source/Structures/Handlers/Events.js
@@ -0,0 +1,29 @@
+const { Events } = require("../Validation/EventNames");
+const { webhooks, botinfo } = require("../../../Configs/main.json");
+const ascii = require("ascii-table");
+
+module.exports = async (client, PG, chalk) => {
+    const webhook = new WebhookClient({
+        url: webhooks.starts
+    });
+    const Table = new ascii("Events Loaded");
+    (await PG(`${(process.cwd().replace(/\\/g, "/"))}/Source/Events/*/*.js`)).map(async (file) => {
+        const event = require(file);
+
+        if (event.name) {
+            if (!Events.includes(event.name)) {
+                const L = file.split("/")
+                return table.addRow(event.name || L[L.length - 2]/L[L.length - 1], "✖")
+            }
+        }
+
+        if(event.once) {
+            client.once(event.name, (...args) => event.execute(...args, client));
+        } else {
+            client.on(event.name, (...args) => event.execute(...args, client));
+        };
+
+        await Table.addRow(event.name, "✔ SUCCESSFUL")
+    });
+    console.log(Table.toString());
+}
