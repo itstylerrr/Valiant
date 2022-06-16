ee8f7caae8858025249e332b3b2a1cf1422e74fe
tyler <63240641+itstylerrr@users.noreply.github.com>
2022-06-16 13:08:15 -0500

    🪵 add logging config cmd

---
+++ Extra/Files/JavaScript/command-template.js
@@ -0,0 +1,38 @@
+const  { CommandInteraction, MessageEmbed, Client } = require('discord.js');
+
+module.exports = {
+    name: "name",
+    description: "description",
+    permission: "permission",
+    options: [
+    ],
+    /**
+     * @param {CommandInteraction} interaction 
+     * @param {Client} client
+     */
+    async execute(interaction, client, data) {
+        // ⟬                    Logging                    ⟭
+
+        if (data.guild.addons.settings.loggingId) {
+            let log = client.channels.cache.get(data.guild.addons.settings.loggingId);
+            let logEmbed = new MessageEmbed()
+            .setTitle(`${interaction.guild.name}'s Command Logging`)
+            .setThumbnail(interaction.guild.iconURL({dynamic: true}))
+            .setDescription(`**This is an automated message sent by <@${client.user.id}>**`)
+            .addFields(
+                { name: "Command Name:", value: `${data.cmd.name}` },
+                { name: "Ran By:", value: `${interaction.user}` },
+                { name: "Timestamp:", value: `<t:${parseInt(interaction.createdTimestamp / 1000)}:R>` }
+            )
+            .setColor("BLURPLE")
+            .setFooter(`Invite ${data.config.botinfo.name}`)
+
+            log.send({
+                embeds: [logEmbed]
+            });
+        }
+
+        // ⟬                    Command                    ⟭
+        return interaction.reply({content: "you suck at this"})
+    }
+}
---
+++ Source/Commands/Admin/logging.js
@@ -0,0 +1,67 @@
+const  { CommandInteraction, MessageEmbed, Client } = require('discord.js');
+
+module.exports = {
+    name: "logging",
+    description: "Select a channel for logging, to remove, run without options.",
+    permission: "ADMINISTRATOR",
+    options: [
+        {
+            name: "channel",
+            description: "Select a channel that you want logs to be sent to.",
+            type: "CHANNEL",
+            channelTypes: ["GUILD_TEXT"]
+        }
+    ],
+    /**
+     * @param {CommandInteraction} interaction 
+     * @param {Client} client
+     */
+    async execute(interaction, client, data) {
+        // ⟬                    Logging                    ⟭
+
+        if (data.guild.addons.settings.loggingId) {
+            let log = client.channels.cache.get(data.guild.addons.settings.loggingId);
+            let logEmbed = new MessageEmbed()
+            .setTitle(`${interaction.guild.name}'s Command Logging`)
+            .setThumbnail(interaction.guild.iconURL({dynamic: true}))
+            .setDescription(`**This is an automated message sent by <@${client.user.id}>**`)
+            .addFields(
+                { name: "Command Name:", value: `${data.cmd.name}` },
+                { name: "Ran By:", value: `${interaction.user}` },
+                { name: "Timestamp:", value: `<t:${parseInt(interaction.createdTimestamp / 1000)}:R>` }
+            )
+            .setColor("BLURPLE")
+            .setFooter(`Invite ${data.config.botinfo.name}`)
+
+            log.send({
+                embeds: [logEmbed]
+            });
+        }
+
+        // ⟬                    Command                    ⟭
+        const channel = interaction.options.getChannel("channel");
+        if (!channel) {
+            data.guild.addons.settings.loggingId = null;
+            data.guild.save();
+            return interaction.reply({
+                embeds: [
+                    new MessageEmbed()
+                    .setTitle("<a:success:971880058371321877> Logging has been removed from the guild.")
+                    .setColor("GREEN")
+                ]
+            });
+        } else if (channel) {
+            data.guild.addons.settings.loggingId = channel.id;
+            data.guild.markModified("addons.settings");
+            await data.guild.save();
+            return interaction.reply({
+                embeds: [
+                    new MessageEmbed()
+                    .setTitle("<a:success:971880058371321877> Logging has been enabled for this server.")
+                    .setDescription(`Channel: ${channel}`)
+                    .setColor("GREEN")
+                ]
+            })
+        }
+    }
+}
