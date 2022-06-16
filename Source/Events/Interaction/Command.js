1cf5e410c16edd3d9ba6ce7d53b317d11ed51eed
tyler <63240641+itstylerrr@users.noreply.github.com>
2022-06-15 21:09:38 -0500

    🤌 Create command event

---
+++ Source/Events/Interaction/Command.js
@@ -0,0 +1,26 @@
+const { Client, CommandInteraction, MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "interactionCreate",
+    /**
+     * 
+     * @param {CommandInteraction} interaction 
+     * @param {Client} client 
+     */
+    async execute(interaction, client) {
+        if (interaction.isCommand || interaction.isContextMenu()) {
+            const command = client.commands.get(interaction.commandName);
+            if(!command) return interaction.reply({embeds: [
+                new MessageEmbed()
+                .setColor("RED")
+                .setDescription("⛔ An error occured while running this command, please contact <@557016470048210964>.")
+            ]}) && client.commands.delete(interaction.commandName);
+
+            if (command.permission && !interaction.member.permissions.has(command.permission)) {
+                return interaction.reply({ content: `You do not have the required permission for this command: \`${interaction.commandName}\`.`, ephemeral: true })
+            }
+
+            command.execute(interaction, client)
+        }
+    }
+}
