7cd83ff9e2828c58ff162700b9f0191cbf7163d3
tyler <63240641+itstylerrr@users.noreply.github.com>
2022-06-22 12:43:40 -0500

    🔓 add DM check to anti scam

--- Source/Events/Message/AntiScam.js
+++ Source/Events/Message/AntiScam.js
@@ -26,8 +26,11 @@ module.exports = {
             )
             .addField("Message Content:", `\`\`\`${message.content}\`\`\``)
             .setTimestamp();
-          await message.author.send({ embeds: [Ex.setDescription(`I have detected a potention scam message. You cannot send scam messages in this server. If you believe this is an error, please contact the server moderators.`)] });
-          await message.guild.channels.cache.get(data.guildLogs).send({embeds: [Ex.setDescription(`I have detected a potention scam message. You can choose what you would like to do next with the member. To time them out, you can use /mod actions timeout <duration>.`)]});
+            if (messag)
+          await message.author.send({ embeds: [Ex.setDescription(`I have detected a potention scam message. You cannot send scam messages in this server. If you believe this is an error, please contact the server moderators.`)] }).catch((err) => {
+            Ex.addField(`DM Sent?`, `No, unable to send a DM.\n\n**Error:**\n\`${err}\``)
+          });
+          await message.guild.channels.cache.get(data.guildLogs).send({embeds: [Ex.setDescription(`I have detected a potention scam message. You can choose what you would like to do next with the member. To time them out, you can use /mod actions timeout <duration>.`).addField("DM Sent?", "Yes, DM has been sent to user regarding the issue.")]});
           return;
         }
       });
