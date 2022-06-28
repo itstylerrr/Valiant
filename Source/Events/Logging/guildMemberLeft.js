9a351468d8b8a1ebc8d4d016e1f71eb2d5b64677
tyler <63240641+itstylerrr@users.noreply.github.com>
2022-06-28 16:14:13 -0500

    ➖ add guild membewr left log

---
+++ Source/Events/Logging/guildMemberLeft.js
@@ -0,0 +1,49 @@
+const { MessageEmbed, Client, GuildChannel, GuildMember } = require("discord.js");
+const DB = require("../../Structures/Database/Schemas/Logging");
+
+module.exports = {
+    name: "guildMemberLeft",
+    /**
+     *
+     * @param {GuildMember} member
+     * @param {Client} client
+     */
+    async execute(member, client) {
+        const guild = member.guild;
+
+        DB.findOne({
+            GuildID: guild.id,
+        }, async (err, data) => {
+            if (err) throw err;
+            if (!data) return;
+            if (!data.guildLogs) return;
+            if (data.guildLogs) {
+                const chan = data.guildLogs;
+                if (guild.channels.cache.has(chan)) {
+                    const correctChannel = guild.channels.cache.get(chan);
+
+                    correctChannel.send({
+                        embeds: [
+                            new MessageEmbed()
+                            .setAuthor({name: `Member Left | ${member.user.tag}`, iconURL: `${member.displayAvatarURL({ dynamic: true })}`})
+                            .setDescription(
+                                `
+                                ${member} (\`${member.id}\`)
+
+                                **Joined Server:**
+                                <t:${parseInt(member.joinedTimestamp / 1000)}:R>
+
+                                **Account Created:**
+                                <t:${parseInt(member.user.createdTimestamp / 1000)}:R>
+                                `
+                            )
+                            .setColor("GREEN")
+                        ]
+                    });
+                } else {
+                    return;
+                }
+            }
+        })
+    },
+  };
