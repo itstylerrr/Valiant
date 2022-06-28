16d7361eedb431a99117af561fa1d76b6051e9e1
tyler <63240641+itstylerrr@users.noreply.github.com>
2022-06-28 16:40:23 -0500

    ⌚ add timestamp

--- Source/Events/Logging/emojiDelete.js
+++ Source/Events/Logging/emojiDelete.js
@@ -49,6 +49,8 @@ module.exports = {
                                 **Changes:**
 
                                 ${changes.join()}
+
+                                **Event Happened:** <t:${parseInt(emojiLog.createdTimestamp / 1000)}:R>
                                 `
                             )
                             .setColor(guild.me.displayHexColor)
